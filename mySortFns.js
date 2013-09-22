97685cc0a6378b9d8700598adf9e6baacbad6fb6
Calvin Fernandez <cfernand@cs.stanford.edu>
2013-09-22 10:12:20 -0700

    tweaks and colors

---
+++ mySortFns.js
@@ -0,0 +1,31 @@
+//  Merge Sort
+function mergeSort(data) {
+  return data;
+} 
+
+//  Quick Sort
+function quickSort(data) {
+  return data;
+}
+
+//  Heap Sort
+function heapSort(data) {
+  return data;
+}
+
+// Bubble Sort
+function bubbleSort(data) {
+  return data;
+}
+
+//  Radix Sort
+function radixSort(data) {
+  // Implement me!
+  return data;
+}
+
+test.run(mergeSort);
+test.run(quickSort);
+test.run(heapSort);
+test.run(bubbleSort);
+test.run(radixSort);
--- sortTest.js
+++ sortTest.js
@@ -1,28 +1,51 @@
 var random = [93, 29, 1, 96, 32, 59, 69, 50, 27, 83, 21, 53, 64, 65, 68, 44, 70, 90, 84, 63, 88, 15, 74, 51, 92, 26, 47, 2, 5, 95, 34, 24, 19, 54, 98, 33, 17, 16, 39, 40, 7, 13, 81, 14, 94, 52, 57, 61, 25, 78, 97, 28, 49, 43, 41, 89, 20, 82, 31, 75, 18, 42, 10, 67, 58, 46, 37, 55, 36, 62, 4, 79, 77, 66, 6, 12, 72, 56, 9, 99, 71, 76, 3, 35, 80, 23, 91, 85, 48, 86, 11, 30, 60, 45, 73, 38, 100, 8, 22, 87];
 
 var test = {
-  run: function(answerFn) {
+  run: function(userFn) {
+
+    var fail = false;
+    var randomCopyUser = [];
+    for (var i = 0; i < random.length; i ++ ) {
+      randomCopyUser.push(random[i]);
+    }
+
+    var randomCopyECMA = [];
+    for (var i = 0; i < random.length; i ++ ) {
+      randomCopyECMA.push(random[i])
+    }
+
+    console.log(" ----------- Start Test ---------- ")
+
     var userStart = new Date;
-    var answer = answerFn.call(this, random);
+    var userSoln = userFn.call(this, randomCopyUser);
     var userStop = new Date;
 
-    var solnStart = new Date;
-    var solution = random.sort(function(a, b) {return a-b}); 
-    var solnStop = new Date;
+    var ecmaStart = new Date;
+    var ecmaSoln = randomCopyECMA.sort(function(a, b) {return a-b}); 
+    var ecmaStop = new Date;
     
-    console.log("User solution completed in " + userStop - userStart + " milliseconds"); 
-    console.log("ECMA Javascript .sort() completed in " + solnStop - solnStart + " milliseconds");
-    console.log("dt " + (userStop - userStart) - (solnStop - solnStart) + " milliseconds"); 
-    
-    if (answer.length !== soluntion.length) {
+    if (ecmaSoln.length !== userSoln.length) {
+      fail = true;
       throw "Arrays are different sizes!";
     }  
 
-    for (var i = 0; i < answer.length; i++) {
-      if (solution[i] !== answer[i]) {
-        throw "Mismatch at array index " + i + ". Value was " + answer[i] + " but was expected to be " + solution[i];
+    for (var i = 0; i < ecmaSoln.length; i++) {
+      if (ecmaSoln[i] !== userSoln[i]) {
+        fail = true;
+        console.log(" %c Mismatch at array index " + i + ". Value was " + userSoln[i] + " but was expected to be " + ecmaSoln[i], 'color: #c7254e');
       }     
     }
+    console.log("");
+    console.log("  User Solution: " + userSoln);
+    console.log("  ECMA Solution: " + ecmaSoln);
+    console.log("");
+
+    if (fail) {
+      console.log( "%c Failed! ", 'color: #c7254e')
+    } else {
+      console.log( "%c Success! ", 'color: #5cb85c');
+    }
+
+    console.log(" ----------- End Test ---------- ")
   }
 };
-
